d97028fbf5515de70529784675d17c21479ddf3d
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-03-04 14:54:54 -0500

    Added ws_char option for finer control

--- R/basic_pattern_analysis.R
+++ R/basic_pattern_analysis.R
@@ -1,5 +1,5 @@
 #' @keywords internal
-get_pattern <- function(x, show_ws = TRUE) {
+get_pattern <- function(x, show_ws = TRUE, ws_char = "w") {
   if (!is.character(x)) {
     x <- as.character(x)
   }
@@ -7,7 +7,7 @@ get_pattern <- function(x, show_ws = TRUE) {
   x <- gsub("[A-Z]", "A", x)
   x <- gsub("[0-9]", "9", x)
   if (show_ws) {
-    x <- gsub("\\s", "w", x)
+    x <- gsub("\\s", ws_char, x)
   }
   x
 }
@@ -20,7 +20,9 @@ get_pattern <- function(x, show_ws = TRUE) {
 #' @param unique_only Logical indicating whether or not to only show the unique
 #'   patterns. Default is \code{TRUE}.
 #' @param show_ws Logical indicating whether or not to show whitespace
-#'   as a \code{"w"} character. Default is \code{TRUE}.
+#'   using a special character. Default is \code{TRUE}.
+#' @param ws_char Character string to use to depict whitespace when 
+#'   \code{show_ws = TRUE}
 #' @param useNA Logical indicating whether to include \code{NA} values in the
 #'   table. See \code{\link{table}} for details.
 #' @param ... Additional optional arguments to be passed onto \code{llply}.
@@ -30,7 +32,7 @@ get_pattern <- function(x, show_ws = TRUE) {
 #' basic_pattern_analysis(iris)
 #' basic_pattern_analysis(iris, unique_only = TRUE)
 basic_pattern_analysis <- function(x, unique_only = FALSE,
-                                   show_ws = TRUE,
+                                   show_ws = TRUE, ws_char = "w",
                                    useNA = c("no", "ifany", "always"), ...) {
   UseMethod("basic_pattern_analysis")
 }
@@ -39,14 +41,14 @@ basic_pattern_analysis <- function(x, unique_only = FALSE,
 #' @rdname bpa
 #' @export
 basic_pattern_analysis.default <- function(x, unique_only = FALSE,
-                                           show_ws = TRUE,
+                                           show_ws = TRUE, ws_char = "w",
                                            useNA = c("no", "ifany", "always"),
                                            ...) {
   useNA <- match.arg(useNA)
   if (unique_only) {
-    table(get_pattern(x, show_ws = show_ws), useNA = useNA)
+    table(get_pattern(x, show_ws = show_ws, ws_char = ws_char), useNA = useNA)
   } else {
-    get_pattern(x, show_ws = show_ws)
+    get_pattern(x, show_ws = show_ws, ws_char = ws_char)
   }
 }
 
@@ -55,12 +57,12 @@ basic_pattern_analysis.default <- function(x, unique_only = FALSE,
 #' @importFrom plyr llply
 #' @export
 basic_pattern_analysis.data.frame <- function(x, unique_only = FALSE,
-                                              show_ws = TRUE,
+                                              show_ws = TRUE, ws_char = "w",
                                               useNA = c("no", "ifany", "always"),
                                               ...) {
   useNA <- match.arg(useNA)
   z <- llply(x, basic_pattern_analysis.default, unique_only = unique_only,
-             show_ws = show_ws, useNA = useNA, ...)
+             show_ws = show_ws, ws_char = ws_char, useNA = useNA, ...)
   if (unique_only) {
     z
   } else {
